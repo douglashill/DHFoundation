5577ab850c91d86d419cc46a2d399b285fde26f8
Douglas Hill <hello@douglashill.co>
2015-05-05 12:24:02 +0100

    Add Xcode project with framework targets
    There are framework targets for OS X and iOS. This should make these utilities easier to work on, test and integrate.

---
+++ Source/DHFoundation.h
@@ -0,0 +1,14 @@
+// Douglas Hill, May 2015
+// https://github.com/douglashill/DHFoundation
+
+#import <DHFoundation/DHExceptionCreation.h>
+#import <DHFoundation/DHIntegerConversion.h>
+#import <DHFoundation/DHMap.h>
+#import <DHFoundation/NSArray+DHAdditions.h>
+#import <DHFoundation/NSArray+DHFold.h>
+#import <DHFoundation/NSDictionary+DHJSONReading.h>
+#import <DHFoundation/NSSet+DHAdditions.h>
+
+#import <DHFoundation/DHCompositeTransformer.h>
+#import <DHFoundation/DHFormatterTransformer.h>
+#import <DHFoundation/DHStringToUTF8DataTransformer.h>
